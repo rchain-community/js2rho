7bccee05d7477b3faf213c8ddc46a792fbadd7b4
Dan Connolly <dckc@madmode.com>
2020-04-22 18:40:06 -0500

    notary: port from E to JS
      - add @agoric/harden dependency

--- examples/notary.js
+++ examples/notary.js
@@ -1,64 +1,65 @@
 // taken from "E in a Walnut" by Marc Stiegler 2000
 // http://wiki.erights.org/wiki/Walnut/Secure_Distributed_Computing/Capability_Patterns#Vouching_with_Notary.2FInspector
 
-# E sample
-####### Widget Inc. software #####
+import harden from '@agoric/harden';
 
-#vouching system 
-#returns a private notary that offers a public inspector
-#throws problem if the object being vouched is not vouchable
-def makeNotary()  {
-    def nonObject {}
-    def unvouchedException(obj) {throw(`Object not vouchable: $obj`)}
-    var vouchableObject := nonObject
-    def inspector {
-        to vouch(obj) {
-            vouchableObject := nonObject
+// E sample
+// ##### Widget Inc. software #####
+
+// vouching system
+// returns a private notary that offers a public inspector
+// throws problem if the object being vouched is not vouchable
+function makeNotary()  {
+    const nonObject = {};
+    function unvouchedException(obj) { throw(`Object not vouchable: $obj`); }
+    let vouchableObject = nonObject;
+    const inspector = harden({
+        vouch(obj) {
+            vouchableObject = nonObject;
             try {
-                obj.startVouch()
-                if (vouchableObject == nonObject) {
-                    return unvouchedException(obj)
+                obj.startVouch();
+                if (vouchableObject === nonObject) {
+                    return unvouchedException(obj);
                 } else {
-                    def vouchedObject := vouchableObject
-                    vouchableObject := nonObject
-                    return vouchedObject
+                    const vouchedObject = vouchableObject;
+                    vouchableObject = nonObject;
+                    return vouchedObject;
                 }
-            } catch err {unvouchedException(obj)}
+            } catch(err) { return unvouchedException(obj); }
         }
-    }
-    def notary {
-        to startVouch(obj) { vouchableObject := obj}
-        to getInspector()  {return inspector}
-    }
-    return notary
+    });
+    const notary = harden({
+        startVouch(obj) { vouchableObject = obj; },
+        getInspector()  { return inspector; }
+    });
+    return notary;
 }
 
-#create Widget Inc's notary
-def widgetNotary := makeNotary()
+// create Widget Inc's notary
+const widgetNotary = makeNotary();
 
-#Order form maker
-def makeOrderForm(salesPerson) {
-    def orderForm {
-        # .... methods for implementing orderForm
-        to startVouch() {widgetNotary.startVouch(orderForm)}
-    }
-    return orderForm
+// Order form maker
+function makeOrderForm(salesPerson) {
+    const orderForm = harden({
+        // .... methods for implementing orderForm
+        startVouch() { widgetNotary.startVouch(orderForm); }
+    });
+    return orderForm;
 }
 
-#publicly available inspector object 
-#(accessible through a uri posted on Widget Inc's web site)
-def WidgetInspectionService {
-    to getInspector() {return widgetNotary.getInspector()}
-}
+// publicly available inspector object
+// (accessible through a uri posted on Widget Inc's web site)
+const WidgetInspectionService = harden({
+    getInspector() { return widgetNotary.getInspector(); }
+});
 
-##### bob software #####
+// ##### bob software #####
 
-#scaffold for sample
-def getOrderFormFromBob()  {return makeOrderForm("scaffold")}
+//  scaffold for sample
+function getOrderFormFromBob()  { return makeOrderForm("scaffold"); }
 
-########## Alice's software to vouch for the order form she received from Bob #####
+// ########## Alice's software to vouch for the order form she received from Bob #####
 
-def untrustedOrderForm := getOrderFormFromBob()
-def inspector := WidgetInspectionService.getInspector()
-def trustedOrderForm := inspector.vouch(untrustedOrderForm)
- 
+const untrustedOrderForm = getOrderFormFromBob();
+const inspector = WidgetInspectionService.getInspector();
+const trustedOrderForm = inspector.vouch(untrustedOrderForm);
