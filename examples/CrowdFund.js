abfad56bede4d06d107e8a92ac660afa81af01e0
Dan Connolly <dckc@madmode.com>
2020-09-25 12:53:25 -0500

    CrowdFund.js: typechecked

--- examples/CrowdFund.js
+++ examples/CrowdFund.js
@@ -1,132 +1,252 @@
 // UNTESTED sketch
-import { E } from "@agoric/eventual-send";
-import REVAddress from "rho:rchain:REVAddress";
+// import { E } from "@agoric/eventual-send";
+// import REVAddress from "rho:rchain:REVAddress";
 import { lookup } from "./lib/registry";
-import { Channel, RhoList, ListOps, RhoMap, tuple } from "./lib/js2rho";
-const { freeze: harden } = Object;
+import { Channel } from "./lib/js2rho";
+const { freeze: harden } = Object; // TODO? @agoric/harden
+
+// TODO: move to lib
+/**
+ * @typedef { { message: string } } Problem
+ *
+ * // TODO: @agoric/nat to handle overflow
+ * @typedef { number } Nat
+ *
+ * @typedef { number } Timestamp
+ */
+
+/**
+ * @typedef { { ok: true, result: T } | { ok: false, problem: Problem } } Result<T>
+ * @template T
+ */
+
+/**
+ * @param { T } result
+ * @returns { Result<T> }
+ * @template T
+ */
+function Ok(result) {
+  return harden({ ok: true, result });
+}
+
+/**
+ * @param {Problem} problem
+ * @returns { Result<T> }
+ * @template T
+ */
+function Err(problem) {
+  return harden({ ok: false, problem });
+}
+
+const ListOps = harden({
+  /**
+   * @param {T[]} items
+   * @param {(item: T) => Promise<Result<U>>} f
+   * @returns { Promise<{ results: U[], problems: Problem[]}> }
+   * @template T
+   * @template U
+   */
+  async mapCollect(items, f) {
+    throw { message: "not impl" };
+  },
+});
+
+/**
+ * clue for js2rho to handle throw
+ * @param { () => Promise<T> } thunk
+ * @returns { Promise<Result<T>>}
+ * @template T
+ */
+async function maybe(thunk) {
+  try {
+    return Ok(await thunk());
+  } catch (err) {
+    return Err(err);
+  }
+}
+
+/**
+ * @typedef {{
+ *   deposit: (allegedPayment: unknown) => Promise<Result<void>>,
+ *   getBalance: () => Promise<Nat>
+ * }} Purse
+ */
+const REVIssuer = harden({
+  /** @type { () => Promise<Purse> } */
+  async makeEmptyPurse() {
+    // TODO: await E(REVAddress).fromUnforgeable(refundKey);
+    const self = harden({
+      /** @type { (pmt: unknown) => Promise<Result<void>> } */
+      async deposit(_pmt) {
+        return Err({ message: "not impl" });
+      },
+      async getBalance() {
+        throw { message: "not impl" };
+      },
+    });
+    return self;
+  },
+});
 
 export default async function main() {
+  /** @type {() => Promise<{ timestamp: Timestamp }> } */
   const blockInfo = await lookup("rho:rchain:blockInfo");
 
   const CrowdFund = harden({
     /**
-     * @param {Int} target pledge target in 10^-8 REV
-     * @param {Int} deadline compared to block timestamp
-     *
-     * @typedef { number } Int
+     * @param {Nat} target pledge target in 10^-8 REV
+     * @param {Timestamp} deadline compared to block timestamp
      */
     make(target, deadline) {
       // TODO: rename to pledges
-      const holdingsCh = Channel({ total: 0, purses: RhoList([]) });
+      /**
+       * @template T
+       * @typedef {{
+       *   peek: () => Promise<T>,
+       *   get: () => Promise<T>,
+       *   put: (p: T) => void
+       * }} ChannelT
+       */
+      /**
+       * TODO: rename purses to payments? or refunds?
+       * @typedef {{total: Nat, purses: Purse[]}} Holdings
+       */
+      /** @type { ChannelT<Holdings>} */
+      const holdingsCh = Channel({ total: 0, purses: [] });
 
-      const beneficiarySeat = harden({
-        async claim(dest) {
-          const { total: pledges } = holdingsCh.peek();
-          if (pledges < target) {
-            return tuple(
-              false,
-              RhoMap({
-                message: "funding target not reached",
+      const check = harden({
+        /**
+         * @param {boolean} met sense of the test
+         * @returns {Promise<Result<Nat>>} current pledges
+         */
+        async pledgeTarget(met) {
+          const { total: pledges } = await holdingsCh.peek();
+          return maybe(async () => {
+            if (pledges >= target === met) {
+              return pledges;
+            } else {
+              throw harden({
+                message: `funding target ${met ? "not" : ""} reached`,
                 pledges,
                 target,
-              })
-            );
-          } else {
-            const { timestamp: now } = await blockInfo();
-            if (now < deadline) {
-              return tuple(
-                false,
-                RhoMap({
-                  message: "payout not available until after deadline",
-                  deadline,
-                  now,
-                })
-              );
-            } else {
-              // commit point
-              const holdings = holdingsCh.get();
-              return ListOps.map(holdings.purses, (pmt) => transfer(pmt, dest));
+              });
             }
+          });
+        },
+        /**
+         * @param {boolean} met sense of the test
+         * @returns {Promise<Result<Timestamp>>} current time
+         */
+        async deadline(met) {
+          const { timestamp: now } = await blockInfo();
+          return maybe(async () => {
+            if (now >= deadline === met) {
+              return now;
+            } else {
+              throw harden({
+                message: `deadline ${met ? "not" : ""} reached`,
+                deadline,
+                now,
+              });
+            }
+          });
+        },
+      });
+
+      /**
+       * @typedef {{
+       *   withdraw: () => Promise<Result<Purse>>
+       * }} ContributorSeatT
+       */
+      const ContributorSeat = harden({
+        /**
+         * @param {Purse} refund
+         * @returns { ContributorSeatT }
+         */
+        make(refund) {
+          return harden({
+            /**
+             * @return {Promise<Result<Purse>>}
+             */
+            async withdraw() {
+              const pledges = await check.pledgeTarget(false);
+              switch (pledges.ok) {
+                case false:
+                  return pledges;
+                case true: {
+                  const now = await check.deadline(true);
+                  switch (now.ok) {
+                    case false:
+                      return now;
+                    case true:
+                      return Ok(refund);
+                  }
+                }
+              }
+            },
+          });
+        },
+      });
+
+      const beneficiarySeat = harden({
+        /**
+         * @returns { Promise<Result<{ benefit: Purse, problems: Problem[] }>> }
+         */
+        async claim() {
+          const pledges = await check.pledgeTarget(true);
+          switch (pledges.ok) {
+            case false:
+              return pledges;
+            case true:
+              const benefit = await REVIssuer.makeEmptyPurse();
+              // commit point
+              const holdings = await holdingsCh.get();
+              const { problems } = await ListOps.mapCollect(
+                holdings.purses,
+                (pmt) => benefit.deposit(pmt)
+              );
+              holdingsCh.put(harden({ total: 0, purses: [] }));
+              return Ok({ benefit, problems });
           }
         },
       });
 
       const publicFacet = harden({
-        status() {
-          const { total: pledges } = holdingsCh.peek();
-          return { terms: { target, deadline }, pledges };
+        /**
+         * @returns { Promise<{terms: {target: Nat, deadline: Timestamp}, pledges: Nat}> }
+         */
+        async status() {
+          const { total: pledges } = await holdingsCh.peek();
+          return harden({ terms: { target, deadline }, pledges });
         },
 
         /**
          * a payment is a vault from unforgeable along with the unforgeable
-         * @param {{ vault: unknown, key: unknown }} pmt
+         * @param {unknown} pmt
+         * @returns {Promise<Result<ContributorSeatT>>}
          */
         async contribute(pmt) {
-          const refundKey = harden({});
-          const refund = await E(REVAddress).fromUnforgeable(refundKey);
-          const [ok, amt] = maybe(await transfer(pmt, refund));
-          if (!ok) {
-            return tuple(false, amt); // transfer failed
-          } else {
-            const contributorSeat = harden({
-              async withdraw() {
-                const { total: pledges } = await holdingsCh.peek();
-                if (pledges >= target) {
-                  return tuple(
-                    false,
-                    RhoMap({
-                      message:
-                        "refund not available after funding target reached",
-                      pledges,
-                      target,
-                    })
-                  );
-                } else {
-                  const { timestamp: now } = await blockInfo();
-                  if (now <= deadline) {
-                    return tuple(
-                      false,
-                      RhoMap({
-                        message: "refund not available until after deadline",
-                        deadline,
-                        now,
-                      })
-                    );
-                  } else {
-                    return tuple(
-                      true,
-                      RhoMap({ vault: refund, key: refundKey })
-                    );
-                  }
-                }
-              },
-            });
-
-            const { total: pledges, purses } = holdingsCh.get();
-            // TODO: handle overflow
-            const total = pledges + amt;
-            const refundPmt = RhoMap({
-              vault: refund,
-              key: refundKey,
-            });
-            holdingsCh(
-              RhoMap({
-                total,
-                purses: purses.append([refundPmt]),
-              })
-            );
-            return tuple(
-              true,
-              RhoMap({
-                message: "pledge accepted",
-                contributorSeat,
-                pledges,
-              })
-            );
+          const refund = await REVIssuer.makeEmptyPurse();
+          const amt = await refund.deposit(pmt);
+          switch (amt.ok) {
+            case false:
+              return amt; // transfer failed
+            case true: {
+              const contributorSeat = ContributorSeat.make(refund);
+              const { total: pledges, purses } = await holdingsCh.get();
+              const p = await refund.getBalance();
+              const more = {
+                // TODO: Nat, maybe to handle overflow
+                total: pledges + p,
+                purses: [...purses, refund],
+              };
+              holdingsCh.put(more);
+              return Ok(contributorSeat);
+            }
           }
         },
       });
-      return RhoMap({ beneficiarySeat, publicFacet });
+      return harden({ beneficiarySeat, publicFacet });
     },
   });
 }
