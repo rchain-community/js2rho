ad88049b7676c1c6f21a670846dac66151be9a25
Dan Connolly <dckc@madmode.com>
2020-09-25 21:52:52 -0500

    CrowdFund: use excape, expect, doubt

--- examples/CrowdFund.js
+++ examples/CrowdFund.js
@@ -1,10 +1,11 @@
 // @ts-check
 // import { E } from "@agoric/eventual-send";
 import { Channel } from "./lib/rspace.js";
-import { Ok, maybe, unwrap } from "./lib/result.js";
+import { Ok, escape, expect, doubt } from "./lib/result.js";
 import * as _registry from "./lib/registry.js";
 // import REVAddress from "rho:rchain:REVAddress";
 import { RevAddress, Nat } from "./lib/rev.js";
+import { parse } from "path";
 
 const { freeze: harden } = Object; // TODO? @agoric/harden
 
@@ -15,6 +16,7 @@ const { freeze: harden } = Object; // TODO? @agoric/harden
  * @typedef { number } Timestamp
  *
  * @typedef { import('./lib/result').Problem} Problem
+ * @typedef { import('./lib/result').Ejector} Ejector
  * @typedef { import('./lib/rev').REVAddress} REVAddress
  */
 /**
@@ -54,15 +56,15 @@ export default async function main({ registry }) {
       let myAddr;
 
       const self = harden({
-        /** @type { (pmt: unknown) => Promise<Result<unknown>> } */
-        async deposit(pmt) {
-          // @ts-ignore getBalan
-          const amount = await pmt.getBalance();
+        /** @type { (allegedPayment: unknown) => Promise<Result<unknown>> } */
+        async deposit(allegedPayment) {
+          const pmtAuthKey = await RevVault.unforgeableAuthKey(allegedPayment);
+          // @ts-ignore
+          const amount = await allegedPayment.getBalance();
           Nat(amount);
           // @ts-ignore
           // could do these next 2 in parallel with Promise.all
-          const pmtAddr = await pmt.getAddress();
-          const pmtAuthKey = await RevVault.unforgeableAuthKey(pmt);
+          const pmtAddr = await allegedPayment.getAddress();
           const result = await myVault.transfer(pmtAddr, amount, pmtAuthKey);
           return result;
         },
@@ -106,40 +108,32 @@ export default async function main({ registry }) {
 
       const check = harden({
         /**
+         * @param {Ejector} ej
          * @param {boolean} met sense of the test
-         * @returns {Promise<Result<NatT>>} current pledges
+         * @param {string} message in case of failure
+         * @returns {Promise<NatT>} current pledges
          */
-        async pledgeTarget(met) {
+        async pledgeTarget(ej, met, message) {
           const { balance: pledges } = await holdingsCh.peek();
-          return maybe(async () => {
-            if (pledges >= target === met) {
-              return pledges;
-            } else {
-              throw harden({
-                message: `funding target ${met ? "not" : ""} reached`,
-                pledges,
-                target,
-              });
-            }
-          });
+          if (pledges >= target === met) {
+            return pledges;
+          } else {
+            ej(harden({ message, pledges, target }));
+          }
         },
         /**
+         * @param {Ejector} ej
          * @param {boolean} met sense of the test
-         * @returns {Promise<Result<Timestamp>>} current time
+         * @param {string} message
+         * @returns {Promise<Timestamp>} current time
          */
-        async deadline(met) {
+        async deadline(ej, met, message) {
           const [_num, now, _sender] = await blockData();
-          return maybe(async () => {
-            if (now >= deadline === met) {
-              return now;
-            } else {
-              throw harden({
-                message: `deadline ${met ? "not" : ""} reached`,
-                deadline,
-                now,
-              });
-            }
-          });
+          if (now >= deadline === met) {
+            return now;
+          } else {
+            ej(harden({ message, deadline, now }));
+          }
         },
       });
 
@@ -151,29 +145,19 @@ export default async function main({ registry }) {
       const ContributorSeat = harden({
         /**
          * @param {Purse} pledge
-         * @returns { ContributorSeatT }
+         * @returns { Promise<ContributorSeatT> }
          */
-        make(pledge) {
+        async make(pledge) {
           return harden({
             /**
              * @return {Promise<Result<Purse>>}
              */
             async withdraw() {
-              const pledges = await check.pledgeTarget(false);
-              switch (pledges.ok) {
-                case false:
-                  // move message back here?
-                  return pledges;
-                case true: {
-                  const now = await check.deadline(true);
-                  switch (now.ok) {
-                    case false:
-                      return now;
-                    case true:
-                      return Ok(pledge);
-                  }
-                }
-              }
+              return escape(async (/** @type Ejector */ ej) => {
+                await check.pledgeTarget(ej, false, `funding target reached`);
+                await check.deadline(ej, true, `deadline not reached`);
+                return pledge;
+              });
             },
           });
         },
@@ -184,21 +168,18 @@ export default async function main({ registry }) {
          * @returns { Promise<Result<{ benefit: Purse, problems: Problem[] }>> }
          */
         async claim() {
-          const pledges = await check.pledgeTarget(true);
-          switch (pledges.ok) {
-            case false:
-              return pledges;
-            case true:
-              const benefit = await REVIssuer.makeEmptyPurse();
-              // commit point
-              const holdings = await holdingsCh.get();
-              const { problems } = await ListOps.mapCollect(
-                holdings.pledges,
-                (pmt) => benefit.deposit(pmt)
-              );
-              holdingsCh.put(harden({ balance: 0, pledges: [] }));
-              return Ok({ benefit, problems });
-          }
+          return escape(async (/** @type { Ejector } */ ej) => {
+            await check.pledgeTarget(ej, true, `funding target not reached`);
+            const benefit = await REVIssuer.makeEmptyPurse();
+            // commit point
+            const holdings = await holdingsCh.get();
+            const { problems } = await ListOps.mapCollect(
+              holdings.pledges,
+              (pmt) => benefit.deposit(pmt)
+            );
+            holdingsCh.put(harden({ balance: 0, pledges: [] }));
+            return { benefit, problems };
+          });
         },
       });
 
@@ -212,7 +193,6 @@ export default async function main({ registry }) {
         },
 
         /**
-         * a payment is a vault from unforgeable along with the unforgeable
          * @param {unknown} pmt
          * @returns {Promise<Result<ContributorSeatT>>}
          */
@@ -223,15 +203,14 @@ export default async function main({ registry }) {
             case false:
               return amt; // transfer failed
             case true: {
-              const contributorSeat = ContributorSeat.make(pledge);
+              const contributorSeat = await ContributorSeat.make(pledge);
               const { balance, pledges } = await holdingsCh.get();
               const pbal = await pledge.getBalance();
-              const more = {
+              holdingsCh.put({
                 // TODO: refund in case of overflow?
                 balance: Nat(balance + pbal),
                 pledges: [...pledges, pledge],
-              };
-              holdingsCh.put(more);
+              });
               return Ok(contributorSeat);
             }
           }
@@ -241,30 +220,49 @@ export default async function main({ registry }) {
     },
   });
 
-  /** @type { (clock: () => Timestamp) => Promise<void> } */
-  async function test(clock) {
+  /** @type { (now: Timestamp) => Promise<unknown> } */
+  async function test(now) {
     console.log("testing");
     const REV = 10 ** 8;
     const DAY = 24 * 60 * 60 * 1000;
-    const now = clock();
     const { beneficiarySeat, publicFacet } = await CrowdFund.make(
       100000 * REV,
       now + 14 * DAY
     );
     console.log("fund:", { beneficiarySeat, publicFacet });
-    console.log("claim early", await beneficiarySeat.claim());
 
-    const ej = (problem) => {
-      throw problem;
-    };
-    /** @type { Purse } */
-    const bobPurse = await REVIssuer.makeEmptyPurse();
-    const bobSeat = unwrap(await publicFacet.contribute(bobPurse), ej);
-    console.log(await bobSeat.withdraw());
-    console.log("claim again", await beneficiarySeat.claim());
+    /**
+     * @param { string } label
+     * @param { boolean } ok
+     * @param {Promise<Result<T>>} p
+     * @template T
+     */
+    async function tc(label, ok, p) {
+      const result = await p;
+      const [t, f] = ok ? ["pass", "FAIL"] : ["FAIL", "pass"];
+      switch (result.ok) {
+        case true:
+          console.log(t, ":", label, result.result);
+          break;
+        case false:
+          console.log(f, ":", label, result.problem);
+          break;
+      }
+    }
+    return escape(async (/* @type { Ejector } */ ej) => {
+      console.log();
+      await tc("claim early", false, beneficiarySeat.claim());
+
+      const bobPurse = await REVIssuer.makeEmptyPurse();
+      console.log("contribute");
+      const bobSeat = expect(ej, await publicFacet.contribute(bobPurse));
+      console.log();
+      await tc("withdraw", false, bobSeat.withdraw());
+      await tc("claim", false, beneficiarySeat.claim());
+    });
   }
 
-  await test(() => Date.now());
+  console.log(await test(Date.now()));
 
   console.log("tests passed! inserting into registry");
   // TODO: run tests; insert only if they pass
