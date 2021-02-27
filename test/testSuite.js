cb475de5b35bc1d653debea5c4666e7e60ceb755
Dan Connolly <dckc@madmode.com>
2021-02-27 15:26:03 -0600

    testSuite: types, indent 2

--- test/testSuite.js
+++ test/testSuite.js
@@ -1,92 +1,134 @@
 // @ts-check
-import * as fs from 'fs';
-import assert from 'assert';
+import { js2rho } from "../lib/js2rho.js";
 
-import './esprimaGlobal.js';
-import { js2rho } from '../lib/js2rho.js';
-
+/**
+ * @param {Rd} examples
+ * @param {Wr} outDir
+ * @typedef { ReturnType<typeof mkRd> } Rd
+ * @typedef { ReturnType<typeof mkWr> } Wr
+ */
 async function run(examples, outDir) {
-    const tests = await load(examples);
-    console.log(Object.keys(tests));
+  const tests = await load(examples);
+  console.log(Object.keys(tests));
 
-    await outDir.mkdir({ recursive: true });
-    for (const entry of Object.entries(tests)) {
-        const [name, info] = entry;
-        console.log('\n==== TEST CASE: ', name);
-        if (!('input' in info)) {
-            console.log('== no input');
-            continue;
-        }
-        console.log('js input:', info.input.content.length, info.input.content.slice(0, 60));
-        const actual = outDir.resolve(name + '.rho');
-        actual.withSyncWriter(ws => js2rho(info.input.content, ws));
-        console.log('\n==== DONE: ', name);
+  await outDir.mkdir({ recursive: true });
+  for (const entry of Object.entries(tests)) {
+    const [name, info] = entry;
+    console.log("\n==== TEST CASE: ", name);
+    if (!("input" in info)) {
+      console.log("== no input");
+      continue;
     }
-    console.log('test suite run DONE.');
+    console.log(
+      "js input:",
+      info.input.content.length,
+      info.input.content.slice(0, 60)
+    );
+    const actual = outDir.resolve(name + ".rho");
+    await actual.withSyncWriter(async (ws) => {
+      try {
+        await js2rho(info.input.content, ws);
+      } catch (compileError) {
+        // console.warn(info.input.name, compileError);
+        console.log("@@lose");
+      }
+    });
+    console.log("\n==== DONE: ", name);
+  }
+  console.log("test suite run DONE.");
 }
 
-export default
-    function unittest(out) {
-}
+export default function unittest(out) {}
 
-
+/**
+ * @param {Rd} examples
+ * @returns {Promise<Record<string, Case>>}
+ * @typedef {{ input?: FileInfo, expected?: FileInfo }} Case
+ * @typedef {{ name: string, content: string }} FileInfo
+ */
 async function load(examples) {
-    const names = await examples.readdir();
-    const cases = {};
-    for (const name of names) {
-        const basename = name.replace(/\..*$/, '');
-        if (!(basename in cases)) {
-            cases[basename] = {};
-        }
-        const info = cases[basename];
-        const content = await examples.resolve(name).readFile();
-        if (name.endsWith('.js')) {
-            info.input = { name, content };
-        }
-        if (name.endsWith('.rho')) {
-            info.expected = { name, content };
-        }
+  const names = await examples.readdir();
+  /** @type { Record<string, Case> } */
+  const cases = {};
+  for (const name of names) {
+    if (!name.includes(".")) {
+      continue;
     }
-    return cases;
+    const basename = name.replace(/\..*$/, "");
+    if (!(basename in cases)) {
+      cases[basename] = {};
+    }
+    const info = cases[basename];
+    const content = await examples.resolve(name).readFile();
+    if (name.endsWith(".js")) {
+      info.input = { name, content };
+    }
+    if (name.endsWith(".rho")) {
+      info.expected = { name, content };
+    }
+  }
+  return cases;
 }
 
+/**
+ * @param {typeof import('fs')} fs
+ * @param {typeof import('fs').promises} fsp
+ * @param {string} path
+ */
 function mkWr(fs, fsp, path) {
-    return Object.freeze({
-        withSyncWriter(thunk) {
-            const buf = [];
-            const writer = { write(data) { buf.push(data); } };
-            let ret;
-            ret = thunk(writer);
-            fs.writeFileSync(path, buf.join(''));
-            return ret;
+  return Object.freeze({
+    /**
+     * @param {(writer: SyncWriter) => Promise<void>} thunk
+     * @typedef { { write: (data: string) => void } } SyncWriter
+     */
+    async withSyncWriter(thunk) {
+      /** @type { string[] } */
+      const buf = [];
+      const writer = {
+        /** @param { string } data */
+        write(data) {
+          buf.push(data);
         },
-        mkdir(options) {
-            return fsp.mkdir(path, options);
-        },
-        resolve(there) {
-            return mkWr(fs, fsp, `${path}/${there}`);
-        },
-        readOnly() {
-            return mkRd(fsp, path);
-        }
-    })
+      };
+      const ret = await thunk(writer);
+      fs.writeFileSync(path, buf.join(""));
+      return ret;
+    },
+    mkdir(options) {
+      return fsp.mkdir(path, options);
+    },
+    /** @param { string } there */
+    resolve(there) {
+      return mkWr(fs, fsp, `${path}/${there}`);
+    },
+    readOnly() {
+      return mkRd(fsp, path);
+    },
+  });
 }
 
+/**
+ * @param {typeof import('fs').promises} fsp
+ * @param {string} path
+ */
 function mkRd(fsp, path) {
-    return Object.freeze({
-        async readFile() {
-            return fsp.readFile(path, { encoding: 'utf-8' });
-        },
-        async readdir() {
-            return fsp.readdir(path);
-        },
-        resolve(there) {
-            return mkRd(fsp, `${path}/${there}`);
-        }
-    })
+  return Object.freeze({
+    async readFile() {
+      return fsp.readFile(path, { encoding: "utf-8" });
+    },
+    async readdir() {
+      return fsp.readdir(path);
+    },
+    resolve(there) {
+      return mkRd(fsp, `${path}/${there}`);
+    },
+  });
 }
 
-
-run(mkRd(fs.promises, 'examples'), mkWr(fs, fs.promises, 'test-results'))
-    .then(_ => console.log('== run DONE.'))
-    .catch(crash => console.error(crash));
+(({ fs }) => {
+  run(mkRd(fs.promises, "examples"), mkWr(fs, fs.promises, "test-results"))
+    .then((_) => console.log("== run DONE."))
+    .catch((crash) => {
+      console.log(crash);
+    });
+})({ fs: require("fs") });
